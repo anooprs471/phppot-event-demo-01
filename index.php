163a9e6a25f48cb749430354342d6353a4a6c5fb
anooprs471 <87162365+anooprs471@users.noreply.github.com>
2023-11-20 14:14:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Event management in php Yup</title>
+<title>Event management in php</title>
 
 <script type="text/javascript"
     src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.js"></script>
